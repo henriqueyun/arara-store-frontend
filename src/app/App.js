ea49d588e6780c2902c219e5eeaef7689319edc0
henriqueyun <henriqueyun@gmail.com>
2023-02-09 00:34:04 -0300

    initial commit

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Menubar from './components/Menubar.js'
+function App() {
+  return (
+    <div className="App">
+      <Menubar></Menubar>
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/components/Menubar.js
@@ -0,0 +1,26 @@
+export default function Menubar() {
+    return (
+        <nav>
+            <h1>
+                ARARASTORE
+            </h1>
+            <input type="text"></input>
+            <ul>
+                <li>
+                    Home▼
+                </li>
+                <li>
+                    Muié▼
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    Carrinho
+                </li>
+                <li>
+                    Entrar
+                </li>
+            </ul>
+        </nav>
+    )
+}
