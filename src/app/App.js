f8123c3be1c151a92fa2fe939bc439bb64288d2e
henriqueyun <henriqueyun@gmail.com>
2023-02-10 00:52:35 -0300

    feat: added Material UI and rebuilded navbar

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,17 @@
+import { ThemeProvider } from '@emotion/react';
 import './App.css';
 import Menubar from './components/Menubar.js'
+import theme from './theme/Theme'
+
 function App() {
   return (
-    <div className="App">
-      <Menubar></Menubar>
-      <header className="App-header">
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Menubar></Menubar>
+        <header className="App-header">
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/app/components/Menubar.js
+++ src/app/components/Menubar.js
@@ -1,26 +1,24 @@
-export default function Menubar() {
+import { Grid, Typography, TextField, Button } from '@mui/material';
+import DropdownMenuitem from './DropdownMenuitem';
+
+export default function MUIMenubar() {
     return (
-        <nav>
-            <h1>
-                ARARASTORE
-            </h1>
-            <input type="text"></input>
-            <ul>
-                <li>
-                    Home▼
-                </li>
-                <li>
-                    Muié▼
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    Carrinho
-                </li>
-                <li>
-                    Entrar
-                </li>
-            </ul>
-        </nav>
+        // todo: ajustar cores no tema padrão (importar theme e por de referência?)
+        <Grid container p={2} justifyContent="space-between" alignItems="center" sx={{ backgroundColor: "#323232", color: "white" }}>
+            <Typography variant="h3">Ararastore</Typography>
+            <TextField size="small" variant="filled"></TextField>
+            <Grid sx={{ display: "flex" }}>
+                <DropdownMenuitem>
+                    Home
+                </DropdownMenuitem>
+                <DropdownMenuitem>
+                    Muié
+                </DropdownMenuitem>
+            </Grid>
+            <Grid sx={{ display: "flex" }}>
+                <Button>Entrar</Button>
+                <Button>Carrinho</Button>
+            </Grid>
+        </Grid>
     )
 }
