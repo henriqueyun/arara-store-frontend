4e724f7bb6f39c62cfa17abcd71a4adea32442c1
henriqueyun <henriqueyun@gmail.com>
2023-05-02 17:04:30 -0300

    feat: changed image from static mock to real link

--- src/app/components/ProductItem.jsx
+++ src/app/components/ProductItem.jsx
@@ -5,9 +5,9 @@ export default function ProductItem(props) {
     return (
         <div>
             <img
-                src="https://images.unsplash.com/photo-1519764622345-23439dd774f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFzaGlvbiUyMGJveXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                src={product.image}
                 alt="Um homem estiloso olhando para a camêra de costas para uma parede preta"
-                style={{ maxWidth: "300px" }}
+                style={{ width: "300px", height: "450px" }}
             ></img>
             <Typography variant="h6">{product.name}</Typography>
             <Price price={product.price} discount={product.discount}></Price>
