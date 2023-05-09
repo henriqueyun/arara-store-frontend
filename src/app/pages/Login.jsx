cee34241f18d27567c9a0e36bd1e28421dd444ed
Felipe Couto <couto.felipe2@gmail.com>
2023-05-09 00:13:01 -0300

    Revert "style: adicionando eslint"
    This reverts commit 5fbfca91fe7323085740b7928dc3896e93de1e0a.

--- src/app/pages/Login.jsx
+++ src/app/pages/Login.jsx
@@ -1,33 +1,32 @@
-import { Button, Container } from '@mui/material';
-import React, { useState } from 'react';
-import { LoginService } from '../../client/services/login';
+import { Button, Container } from "@mui/material";
+import { LoginService } from "../../client/services/login";
+import { useState } from "react";
 
 export default function Login() {
   const loginService = new LoginService();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [email, setEmail] = useState()
 
   async function login() {
-    await loginService.login({ login: email, password });
+    await loginService.login({login: email, password: password})
   }
 
   return (
-    <Container maxWidth="xl">
+     <Container maxWidth="xl">
       <Button onClick={login()}>
-        <form action="">
-          <label htmlFor="username">
-            <p>Username</p>
-            <input type="text" id="username" />
-          </label>
-          <label htmlFor="password">
-            <p>Password</p>
-            <input type="password" id="password" />
-          </label>
-          <div>
-            <button type="submit">Login</button>
-          </div>
-        </form>
+    <form action="">      
+      <label>
+        <p>Username</p>
+        <input type="text" />
+      </label>
+      <label>
+        <p>Password</p>
+        <input type="password" />
+      </label>
+      <div>
+        <button type="submit">Login</button>
+      </div>
+    </form>
       </Button>
-    </Container>
-  );
+     </Container>
+  )
 }
