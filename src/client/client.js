7afdb512471bc0f7c71379825dfc47610a537334
Felipe Couto <couto.felipe2@gmail.com>
2023-05-20 22:45:47 -0300

    feature: interceptando response e apagando token caso seja 401

--- src/client/client.js
+++ src/client/client.js
@@ -2,6 +2,25 @@ import axios from 'axios';
 import { ProductService, AuthService } from './services';
 
 axios.defaults.baseURL = 'http://localhost:3000';
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async function (error) {
+    const originalRequest = error.config;
+
+    if (
+      error.response.status === 401 &&
+      originalRequest.url === 'http://localhost:3000/login/'
+    ) {
+      localStorage.removeItem('token')
+      // TODO: mandar para o login aqui
+      return Promise.reject(error);
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 const client = {
   products: ProductService(axios),
