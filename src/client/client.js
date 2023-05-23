777f4755e6da3ccf5aa22266b2159957377109ae
Felipe Couto <couto.felipe2@gmail.com>
2023-05-23 02:41:00 -0300

    fix: colocando defaults do axios em uma func

--- src/client/client.js
+++ src/client/client.js
@@ -2,22 +2,26 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { ProductService, AuthService } from './services';
 
-axios.defaults.baseURL = 'http://localhost:3000';
-axios.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async function (error) {
-    const navigate = useNavigate();
-    if (error.response.status === 401) {
-      localStorage.removeItem('token');
-      navigate('/login');
-      return Promise.reject(error);
-    }
+function setAxiosConfig() {
+  axios.defaults.baseURL = 'http://localhost:3000';
+  axios.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async function (error) {
+      const navigate = useNavigate();
+      if (error.response.status === 401) {
+        localStorage.removeItem('token');
+        navigate('/login');
+        return Promise.reject(error);
+      }
 
-    return Promise.reject(error);
-  },
-);
+      return Promise.reject(error);
+    },
+  );
+}
+
+setAxiosConfig();
 
 const client = {
   products: ProductService(axios),
